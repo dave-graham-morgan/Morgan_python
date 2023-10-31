3cd604f814e4600e25910d1975bc7d2e987ed070
Dave Morgan <dave.g.morgan@gmail.com>
2023-10-31 12:02:25 -0600

    completed part two of SQLAlchmey homework

--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -22,6 +22,21 @@ class User(db.Model):
    last_name = db.Column(db.String(50), 
                   nullable = False,
                   unique = False)
-   image_url = db.Column(db.String(1000),
+   image_url = db.Column(db.String(10000),
                   nullable = True,
                   unique = False)
+   posts = db.relationship('Post', backref='author',lazy=True)
+                           
+
+class Post(db.Model):
+   """one to many posts to users"""
+   __tablename__ = "posts"
+   id = db.Column(db.Integer,
+                  primary_key = True,
+                  autoincrement=True)
+   title = db.Column(db.String(300),
+                     nullable = False)
+   content = db.Column(db.String(10000))
+   user_id = db.Column(db.Integer, 
+                       db.ForeignKey('users.id'),
+                       nullable=False)
