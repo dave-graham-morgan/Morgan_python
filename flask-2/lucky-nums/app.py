790e2933e9a7b89720ee45e528c54aa0d8d625f0
Dave Morgan <dave.g.morgan@gmail.com>
2023-11-27 13:07:05 -0700

    extra credit flask2 assignment complete

--- flask-2/lucky-nums/app.py
+++ flask-2/lucky-nums/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests, random
+
 
 app = Flask(__name__)
 
@@ -8,3 +10,48 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+@app.route("/api/get-lucky-num", methods=["POST"])
+def get_lucky_num():
+    data = request.json
+    name = data.get('name')
+    
+    print(f"name: {name}")
+    email = data.get('email')
+    year = data.get('year')
+    color = data.get('color')
+
+    errors = {}
+    if not name:  
+        errors["name"] = ["Name is required"]
+    if not email:
+        errors["email"] = ["Email is required"]
+    if not year:
+        errors["year"] = ["Year is required"]
+    if not color:
+        errors["color"] = ["Color is required"]
+    if errors:
+        return jsonify(errors = errors)
+    else:
+        num = random.randrange(0,100) 
+        number_fact = requests.get('http://numbersapi.com/random').text
+        year_fact = requests.get(f'http://numbersapi.com/{year}').text
+
+        data={
+            "num":{
+                "fact":number_fact,
+                "num":num
+            },
+            "year":{
+                "fact":year_fact,
+                "year":year
+            }
+        }
+        return jsonify(data)
+ 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
--- flask-2/lucky-nums/static/lucky.js
+++ flask-2/lucky-nums/static/lucky.js
@@ -1,11 +1,64 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
 function processForm(evt) {
+   if (evt){
+      evt.preventDefault();
+      let name = $('#name');
+      let year = $('#year');
+      let email = $('#email');
+      let color = $('#color');
+
+      let formData = {
+         name: name.val(),
+         year: year.val(),
+         email: email.val(),
+         color: color.val()
+      };
+      console.log(formData);
+
+      $.ajax({
+         url: '/api/get-lucky-num',
+         method: 'POST',
+         contentType: 'application/json',
+         data: JSON.stringify(formData),
+         success: function(data){
+            handleResponse(data);
+         }
+      });
+   }
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
+   const $colorErr = $("#color-err");
+   $colorErr.text('');
+   const $emailErr = $("#email-err");
+   $emailErr.text('');
+   const $nameErr = $("#name-err");
+   $nameErr.text('');
+   const $yearErr = $("#year-err")
+   $yearErr.text('')
+
+   if (resp.errors){
+      if(resp.errors.color){
+         $colorErr.text(resp.errors.color)
+      }
+      if(resp.errors.email){
+         $emailErr.text(resp.errors.email)
+      }
+      if(resp.errors.name){
+         $nameErr.text(resp.errors.name)
+      }
+      if(resp.errors.year){
+         $yearErr.text(resp.errors.year)
+      }
+   }else{
+      const results = $("#lucky-results");
+      results.append(`Your lucky number is ${resp.num.num} (${resp.num.fact})`)
+      results.append(`<br> Your birth year (${resp.year.year}) fact is: ${resp.year.fact}`)
+
+   }
 }
 
 
